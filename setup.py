b109f6e6d5aa444c92757aef1747b858f2de6889
Panagiotis Anagnostou <panagno@uth.gr>
2023-05-23 15:50:35 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 setuptools.setup(
     name="HiPart",
