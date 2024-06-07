d18671c691995539a972ccbcee49911651422e87
Panagiotis Anagnostou <panagno@uth.gr>
2024-06-07 13:23:10 +0300

    Version change for the last bug checks

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 setuptools.setup(
     name="HiPart",
