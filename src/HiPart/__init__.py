9e5f36fc3fbe269678d11aea6ce1cca85086ae52
Panagiotis Anagnostou <panagno@uth.gr>
2024-10-29 14:42:02 +0200

    Version update

--- src/HiPart/__init__.py
+++ src/HiPart/__init__.py
@@ -37,7 +37,7 @@ from KDEpy.NaiveKDE import NaiveKDE
 from KDEpy.TreeKDE import TreeKDE
 from KDEpy.FFTKDE import FFTKDE
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 __author__ = "Panagiotis Anagnostou"
 
 TreeKDE = TreeKDE
